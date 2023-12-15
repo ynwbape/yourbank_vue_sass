2aa973342ec81092b2a849310134c295b1c992da
Pierre nangi <71530777+ynwbape@users.noreply.github.com>
2023-12-15 16:36:03 +0100

    CTA section doned

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,12 @@
 <script setup>
-    import HeroSection from '../components/Home/Herosection.vue'
+    import HeroSection from '../components/Home/Herosection.vue';
+    import CTA from '../components/Standalone/CTA.vue';
 </script>
 
 <template>
     <main>
         <HeroSection />    
+        <CTA />
     </main>    
 </template>
 
@@ -14,6 +16,8 @@
     main {
         display: flex;
         flex-direction: column;
+        align-items: center;
         gap: 15rem;
+        overflow: hidden;
     }
 </style>
