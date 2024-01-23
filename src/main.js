956fc411e9f7e48c5e480d526d4aec1bf99be417
Pierre NANGI <pierre.nangi@outlook.com>
2024-01-23 18:03:39 +0100

    Fonawesome import erased from main.js

--- src/main.js
+++ src/main.js
@@ -3,15 +3,6 @@ import './assets/stylesheets/main.scss';
 import App from './App.vue';
 import router from './router/router';
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-
-/* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
-/* import specific icons */
-import { fas } from '@fortawesome/free-solid-svg-icons';
-
-/* add icons to the library */
-library.add(fas)
-
-createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App)
+.use(router)
+.mount('#app');
