01cf453eae408661e3342d590b4a72d723e61c21
Pierre NANGI <pierre.nangi@outlook.com>
2024-02-06 22:33:59 +0100

    hosting initialized w/ Fireabse

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBSIdVCqEI3M9w5SLpm6SsYrdA4oD0dzm8",
+  authDomain: "yourbank-2b2f6.firebaseapp.com",
+  projectId: "yourbank-2b2f6",
+  storageBucket: "yourbank-2b2f6.appspot.com",
+  messagingSenderId: "28565652322",
+  appId: "1:28565652322:web:372035721be6ab8fbe623b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
